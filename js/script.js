487ace8076706fa2369dd9ba55f05701f9e2915c
kacpibanys <kacpibanys@gmail.com>
2021-07-18 18:38:12 +0200

    poprawiony kod bez powtorzen

--- js/script.js
+++ js/script.js
@@ -6,40 +6,34 @@ function titleClickHandler(event){
   event.preventDefault();
   
   
-  const activeLinks = document.querySelectorAll('.titles a.active');
+  
   const activeLinks = document.querySelectorAll('.titles a.' + ACTIVE_CLASS);
 
   for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+    
     activeLink.classList.remove(ACTIVE_CLASS);
   }
   
 
   const clickedElement = this;
-  activeLink.clickedElement.add('active');
-  
-  
-  const activeArticles = document.querySelectorAll('.titles .active');
+ 
   clickedElement.classList.add(ACTIVE_CLASS);
 
   const activeArticles = document.querySelectorAll('.posts .post.' + ACTIVE_CLASS);
 
   for(let activeArticle of activeArticles){
 
-    activeArticle.classList.remove('active');
+    
     activeArticle.classList.remove(ACTIVE_CLASS);
   }
 
   
   const articleSelector = clickedElement.getAttribute('href');
-  console.log('articleSelector');
+  
 
   
   const targetArticle = querySelector(href);
 
- 
-  activeLink.targetArticle.add('active');
-  console.log('targetArticle');
   const targetArticle = document.querySelector(articleSelector);
 
   targetArticle.classList.add(ACTIVE_CLASS);
@@ -47,4 +41,6 @@ function titleClickHandler(event){
 
 const links = document.querySelectorAll('.titles a');
 
-generateTitleLinks();
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
